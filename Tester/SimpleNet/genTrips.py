d384e08070fda21b0a9f140c517afa6a5f8fd02e
Karsten Jakobsen <karstenjjakobsen@gmail.com>
2012-09-11 16:16:23 +0200

    updated the map

--- Tester/SimpleNet/genTrips.py
+++ Tester/SimpleNet/genTrips.py
@@ -4,14 +4,14 @@ if sys.argv[1] == "":
 	print "Please enter the name of your files"
 	sys.exit()
 
-noTrips = 30
-places = [['Main1toJu1', 'Ju3toOst1']]
+noTrips = 500
+places = [['Main1toJu1', 'Ju3toOst1'],['Main1toJu1', 'Ju3toOst1'],['Main1toJu1', 'Ju2toS2'],['Main1toJu1', 'Ju1toS1'],['Ost1toJu3', 'Ju1toMain1'],['Ost1toJu3', 'Ju1toMain1'],['Ost1toJu3', 'Ju1toS1'],['N1toJu1', 'Ju1toS1'],['N2toJu2', 'Ju2toS2'],['S1toJu1', 'Ju1toN1']]
 
 trips = open(sys.argv[1]+".xml", "w")
 
 print >> trips, "<trips>"
 TID=0 
-for i in range(0,noTrips, 10):
+for i in range(0,noTrips, 2):
 	placeID = int(random.uniform(0,len(places)))
 	
 	print >> trips, '	<trip id="%i" from="%s" to="%s" depart="%i"/>' % (TID,places[placeID][0], places[placeID][1], i)
