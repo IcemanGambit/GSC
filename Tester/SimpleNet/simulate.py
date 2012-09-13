c05589e5af6ca870a8b1bf485e9e77aca2336ed5
Karsten Jakobsen <karstenjjakobsen@gmail.com>
2012-09-13 11:22:42 +0200

    working on testing cars

--- Tester/SimpleNet/simulate.py
+++ Tester/SimpleNet/simulate.py
@@ -10,6 +10,7 @@ process = subprocess.Popen("%s -c %s" % (sumoBinary, sys.argv[1] + "/Data.sumocf
 traci.init(PORT)
 
 step = 0
+GSCvehIds["0"]
 
 while step==0 or traci.simulation.getMinExpectedNumber() > 0:
 	traci.simulationStep()
@@ -17,6 +18,9 @@ while step==0 or traci.simulation.getMinExpectedNumber() > 0:
 	if step in range(0,1):
 		print GSC.TrafficLight.getNextGreen("Ju1", "Main1toJu1.-30_2", "Ju1toN1_0", 400000)
 	
+	for v in traci.vehicle.getIDList()
+		if v in GSCvehIds
+			traci.vehicle.setMaxSpeed(v,GSC.Vehivle.getRecommentedSpeed(v))
 		
 	step+=1
 
