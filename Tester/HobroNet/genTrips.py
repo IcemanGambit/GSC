e50ef04240c1eb91a6c77204ea0835e76cef97ef
Sabrine <smouri08@student.aau.dk>
2012-09-28 08:46:27 +0200

    working on traffic lights

--- Tester/HobroNet/genTrips.py
+++ Tester/HobroNet/genTrips.py
@@ -4,14 +4,14 @@ if sys.argv[1] == "":
 	print "Please enter the name of your files"
 	sys.exit()
 
-noTrips = 0
-places = [['Main1toJu1', 'Ju3toOst1']]
+noTrips = 4
+places = [['1_S', '18_N']]
 
 trips = open(sys.argv[1]+"/trips.xml", "w")
 
 print >> trips, "<trips>"
 TID=0 
-interval = 2
+interval = 1
 for i in range(0,noTrips*interval, interval):
 	placeID = int(random.uniform(0,len(places)))
 	
