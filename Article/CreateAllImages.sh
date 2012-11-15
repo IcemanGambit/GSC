1bd6d8c775fac4200c84a291bc9501ec554294d6
Karsten Jakobsen <karstenjjakobsen@gmail.com>
2012-11-15 10:41:28 +0100

    working on creating many graphs

--- Article/CreateAllImages.sh
+++ Article/CreateAllImages.sh
@@ -3,4 +3,9 @@
 python img.py fuel fulegraf | gnuplot
 python img.py fuel fulegraf2 | gnuplot
 
+python imgGen.py distance_0 distance_0 | gnuplot
+python imgGen.py distance_10 distance_10 | gnuplot
+python imgGen.py distance_50 distance_50 | gnuplot
+python imgGen.py distance_100 distance_100 | gnuplot
+
 
