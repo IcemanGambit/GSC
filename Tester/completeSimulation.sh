9b36b95330194af6518231f99df509d9c79a6dad
Karsten Jakobsen <karstenjjakobsen@gmail.com>
2012-11-20 14:11:58 +0100

    final commit before running test

--- Tester/completeSimulation.sh
+++ Tester/completeSimulation.sh
@@ -8,12 +8,3 @@
 ./run.sh HobroNet -u 50 -o tp5
 ./run.sh HobroNet -u 100 -o tp5
 
-./run.sh HobroNet -tp 10 -u 0 -o tp10
-./run.sh HobroNet -u 10 -o tp10
-./run.sh HobroNet -u 50 -o tp10
-./run.sh HobroNet -u 100 -o tp10
-
-./run.sh HobroNet -tp 20 -u 0 -o tp20
-./run.sh HobroNet -u 10 -o tp20
-./run.sh HobroNet -u 50 -o tp20
-./run.sh HobroNet -u 100 -o tp20
