ab921d5211d27ee669699334a785bb12f3564ea6
Sabrine <smouri08@student.aau.dk>
2012-12-05 13:57:56 +0100

    moved traffic light phase

--- createAllImages.sh
+++ createAllImages.sh
@@ -37,20 +37,13 @@ python imgGen.py uncontrolled_fuelTotal_50 fuelTotalUncontrolled50.png $i | gnup
 python imgGen.py combinedFuel combinedFuel.png $i | gnuplot
 python imgGen.py combinedTime combinedTime.png $i | gnuplot
 
-<<<<<<< HEAD
 #DIRS=`ls -l "TestResults/tp0/10" | egrep '^d' | awk '{print $9}'`
 #for r in $DIRS
 #do
-#	python imgGen.py $r'_combinedRouteTime' $r'_combinedRouteTime.png' tp0 | gnuplot
+#	python imgGen.py $r'_combinedRouteTime' $r'_combinedRouteTime.png' $i | gnuplot
 #done
-=======
-DIRS=`ls -l "TestResults/tp0/10" | egrep '^d' | awk '{print $9}'`
-for r in $DIRS
-do
-	python imgGen.py $r'_combinedRouteTime' $r'_combinedRouteTime.png' $i | gnuplot
-done
+
 python imgGen.py _combinedRouteTime combinedRouteTime.png $i | gnuplot
->>>>>>> 4021b2cb1e2c6b9ef7548909cad92ee4bad2c661
 
 python imgGen.py stops_0 stops0.png $i | gnuplot
 python imgGen.py stops_10 stops10.png $i | gnuplot
