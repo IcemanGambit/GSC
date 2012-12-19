f36608f4374d1065d1e6f8b558a5dfe9fdf7780c
Sabrine <smouri08@student.aau.dk>
2012-12-19 09:36:33 +0100

    clean up of Vehicle.py + wrote stuff i guess

--- Tester/GSC/Vehicle.py
+++ Tester/GSC/Vehicle.py
@@ -4,21 +4,9 @@ import traci
 import TrafficLight, Route
 
 """
-	getGreenSpans(string, int) -> [[int,int],[int,int],...]
+	getRecommentedSpeed(string, int, int) -> int
 
-	returns the timeframes of green light for the next intersection
-"""
-def _getGreenSpans(vhId, maxtime):	
-	TL = _getNextTrafficLight(vhId)
-	if TL:
-		g =TrafficLight.getNextGreen(TL[0],TL[1], TL[2], maxtime)
-		return g
-	return []
-	
-"""
-	getRecommentedSpeed(string, int) -> int
-
-	returns the recommented speed to reach the green light for next intersection within maxtime simulation steps
+	Returns the recommented speed to reach a green light for the next connection within mindistance and maxtime simulation steps
 """
 def getRecommentedSpeed(vhId,minDistance, maxtime):
 	distance = _getDistanceNextTrafficLight(vhId)
@@ -27,28 +15,24 @@ def getRecommentedSpeed(vhId,minDistance, maxtime):
 	t = traci.simulation.getCurrentTime()
 	maxSpeed = traci.lane.getMaxSpeed(traci.vehicle.getLaneID(vhId))
 	
+	#Vehicle is on a connection or there is an induction loop ahead. 
 	if traci.vehicle.getRoadID(vhId).find("Ju_") >= 0 or TrafficLight.inductionLoopAhead(vhId):
 		return maxSpeed
+		
 
-
-	#If there are no more traffic lights on route or
-	#traffic light too far away
-	# -> drive at max speed of the road
+	#If there are no more traffic lights on route or the traffic light is too far away
+	# -> drive at max speed
 	if distance == None or distance >= minDistance:
 		return maxSpeed
 
-	#Calculate optimal speed
+	#Calculate recommended speed
 	smax = 0 #Reaching just as light changes to green
 	smin = 0 #Reaching just before light changes to red
+	
 	for span in spans:
-		deltaTbegin = (span[0] - t) # + 1
-		deltaTend =  (span[1] - t) #- 1
-		
-		#If first span has passed
-		# -> look at next span
-		#if t >= span[1]:# or deltaTend <= 0:
-		#	continue
-		
+		deltaTbegin = (span[0] - t)
+		deltaTend =  (span[1] - t)
+				
 		#If slowest speed is larger than max speed
 		# -> look at next span
 		smin = distance/(deltaTend/1000)
@@ -59,29 +43,40 @@ def getRecommentedSpeed(vhId,minDistance, maxtime):
 		if(deltaTbegin <= 0):
 			smax = maxSpeed 					#Drive as fast possible
 		else:
-			smax = distance/(deltaTbegin/1000)	#Set speed to reach when it changes
-
-		#print  str(deltaTbegin) + "\t" +  str(deltaTend)
-		#print str(smin) + "\t" + str(smax)
+			smax = distance/(deltaTbegin/1000)	#Set speed to reach connection when it changes
 		
-		#Only drive at max speed and not slower than 0
+		#Only drive at max speed
 		if smax > maxSpeed:
 			smax = maxSpeed
-		if smax < 0:
-			smax = 0
-		
-		#We can reace the timespan before it goes red
-		if smax < 4.1: #add minimumspeed of 15km/h
+					
+		#Always recommend at least 15 km/h ~ 4.1 m/s
+		if smax < 4.1:
 			return 4.1
+			
 		return smax
 
 	#No traffic light ahead
 	return maxSpeed
 
 """
+	_getGreenSpans(string, int) -> [[int,int],[int,int],]
+
+	Gets the next connection (traffic light) on route, and returns the time spans when it is green.
+	Returned result is a list of pairs og start and end times represended as seconds since the simulation started.
+"""
+def _getGreenSpans(vhId, maxtime):	
+	TL = _getNextTrafficLight(vhId)
+	if TL:
+		return TrafficLight.getNextGreen(TL[0],TL[1], TL[2], maxtime)
+	return []
+
+
+"""
 	getNextTrafficLight(string) > [string, string, string]
 
-	returns the next intersection puls the incomming and outgoing egdes on the route
+	Returns the next traffic light id, the incomming and outgoing egdes on the route
+	The traffic light is visisted to reduce the complexity of getTrafficLightsOnRoute(vhId)
+
 """
 def _getNextTrafficLight(vhId):
 	egde = traci.vehicle.getRoadID(vhId)
@@ -97,22 +92,26 @@ def _getNextTrafficLight(vhId):
 """
 	getDistanceNextTraficLight(string) -> int
 
-	returns the euclidean distance to next intersection  
+	Returns the euclidean distance to the next traffic light, None if no light ahead.
 """
-#TODO this might need fix to road length
 def _getDistanceNextTrafficLight(vhId):
 	TL = _getNextTrafficLight(vhId)
 	if not TL:
 		return None
-	TL_cord = traci.junction.getPosition(TL[0]) #note that Junction and traficlight is not the same but have identical id
+	TL_cord = traci.junction.getPosition(TL[0]) #Note that junction and traffic light is not the same but have identical id's
 	Vh_cord = traci.vehicle.getPosition(vhId)
-	#print TL[0]
-	#print TrafficLight.getRadius(TL[0])
+
 	return math.sqrt(((TL_cord[0]-Vh_cord[0])**2) + ((TL_cord[1]-Vh_cord[1])**2)) - TrafficLight.getRadius(TL[0])  #-20
 
 """
 	getTotalDistanceDriven(string) -> int
 
+	Note: This is not avaiable through TraCI, and we therefore made a hack. 
+		This function must be called at each simulation step in order to function.
+	
+	The total distance driven is recoded in _previousDistance for each vehicle where first element is the distance, the second is the previous egde and the third is the previous lane.
+	The route contain road ids, and only lanes have a length.
+	getLanePosition(vhId) returns how far the vehicle has driven on this lane.
 """
 _previousDistance = {}
 def getTotalDistanceDriven(vhId):
@@ -128,10 +127,3 @@ def getTotalDistanceDriven(vhId):
 		_previousDistance[vhId][2] = laneID
 
 	return _previousDistance[vhId][0] + traci.vehicle.getLanePosition(vhId)
-
-
-
-
-
-
-
